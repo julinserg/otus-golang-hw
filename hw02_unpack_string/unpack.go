0cf6fd643b007ae0344dac32d7cf3993afc4bdf2
julin.serg <julin.serg@gmail.com>
2022-06-10 18:43:17 +0300

    fix nestif

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -9,40 +9,52 @@ import (
 
 var ErrInvalidString = errors.New("invalid string")
 
+func buildOneSymbol(prevSymbol rune, prevSymbolExist bool, strBuilder *strings.Builder) {
+	if prevSymbolExist {
+		strBuilder.WriteRune(prevSymbol)
+	}
+}
+
+func buildSequenceSymbols(symbol rune, prevSymbol rune, prevSymbolExist bool, strBuilder *strings.Builder) error {
+	if !prevSymbolExist {
+		return ErrInvalidString
+	}
+
+	digit, err := strconv.Atoi(string(symbol))
+	if err != nil {
+		return err
+	}
+
+	if digit != 0 {
+		for i := 0; i < digit; i++ {
+			strBuilder.WriteRune(prevSymbol)
+		}
+	}
+	return nil
+}
+
 func Unpack(str string) (string, error) {
 	if len(str) == 0 {
 		return "", nil
 	}
 	var resultStrBuilder strings.Builder
-	var prevS rune
-	prevSExist := false
-	for _, s := range str {
-		if unicode.IsDigit(s) {
-			if !prevSExist {
-				return "", ErrInvalidString
-			}
-
-			digit, err := strconv.Atoi(string(s))
+	var prevSymbol rune
+	prevSymbolExist := false
+	for _, symbol := range str {
+		if unicode.IsDigit(symbol) {
+			err := buildSequenceSymbols(symbol, prevSymbol, prevSymbolExist, &resultStrBuilder)
 			if err != nil {
 				return "", err
 			}
-
-			if digit != 0 {
-				for i := 0; i < digit; i++ {
-					resultStrBuilder.WriteRune(prevS)
-				}
-			}
-			prevSExist = false
+			prevSymbolExist = false
 		} else {
-			if prevSExist {
-				resultStrBuilder.WriteRune(prevS)
-			}
-			prevS = s
-			prevSExist = true
+			buildOneSymbol(prevSymbol, prevSymbolExist, &resultStrBuilder)
+			prevSymbol = symbol
+			prevSymbolExist = true
 		}
 	}
-	if prevSExist {
-		resultStrBuilder.WriteRune(prevS)
+	if prevSymbolExist {
+		resultStrBuilder.WriteRune(prevSymbol)
 	}
 
 	return resultStrBuilder.String(), nil
