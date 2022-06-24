c61d6fc51f51d03d87ef31dd0c5faef10b0e9e27
julin.serg <julin.serg@gmail.com>
2022-06-24 17:37:21 +0300

    begin

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,8 +17,56 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	m_front *ListItem
+	m_back  *ListItem
+	m_size  int
+}
+
+func (l list) Len() int {
+	return l.m_size
+}
+
+func (l list) Front() *ListItem {
+	return l.m_front
+}
+
+func (l list) Back() *ListItem {
+	return l.m_back
+}
+
+func (l list) PushFront(v interface{}) *ListItem {
+	l.m_size++
+	node := new(ListItem)
+	node.Value = v
+	node.Next = l.m_front
+	node.Prev = nil
+	l.m_front.Prev = node
+	return node
+}
+
+func (l list) PushBack(v interface{}) *ListItem {
+	l.m_size++
+	node := new(ListItem)
+	node.Value = v
+	node.Next = nil
+	node.Prev = l.m_back
+	l.m_back.Next = node
+	return node
+}
+
+func (l list) Remove(i *ListItem) {
+	l.m_size--
+	if i.Prev == nil {
+		l.m_front.Next.Prev = nil
+		l.m_front = l.m_front.Next
+	} else if i.Next == nil {
+		l.m_back.Prev.Next = nil
+		l.m_back = l.m_back.Prev
+	}
+}
+
+func (l list) MoveToFront(i *ListItem) {
+
 }
 
 func NewList() List {
