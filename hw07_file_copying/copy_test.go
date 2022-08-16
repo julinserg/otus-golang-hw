97a32e7a007425ea4db02fa553ce353cf51447a2
julin.serg <julin.serg@gmail.com>
2022-08-16 10:44:44 +0300

    HW07

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,24 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	var from, to string
+	var limit, offset int64
+
+	from = "testdata/input.txt"
+	to = "out.txt"
+	err := Copy(from, to, offset, limit)
+	require.Nil(t, err)
+
+	srcFileStat, err := os.Stat(from)
+	require.Nil(t, err)
+	dstFileStat, err := os.Stat(to)
+	require.Nil(t, err)
+	require.Equal(t, srcFileStat.Size(), dstFileStat.Size())
 }
