634cc681878c479dae1b5e8114d6fb7a582c9ada
julin.serg <julin.serg@gmail.com>
2022-08-17 20:02:56 +0300

    first commit

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,40 @@
 package main
 
+import (
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
+	if len(cmd) < 1 {
+		log.Fatal("name command not pass")
+	}
+
+	execCmd := exec.Command(cmd[0], cmd[1:]...)
+	execCmd.Env = append(os.Environ(),
+		"USER=petya",
+		"CITY=SPb",
+	)
+
+	execCmd.Stdout = os.Stdout
+
+	err := execCmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Waiting for command to finish...")
+	if err := execCmd.Wait(); err != nil {
+		if exiterr, ok := err.(*exec.ExitError); ok {
+			if status, ok := exiterr.Sys().(syscall.WaitStatus); ok {
+				log.Printf("Exit Status: %d", status.ExitStatus())
+				return status.ExitStatus()
+			}
+		} else {
+			log.Fatalf("execCmd.Wait: %v", err)
+		}
+	}
 	return
 }
