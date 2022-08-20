f607727050ab46777fd5869544ad07ef47f482b8
julin.serg <julin.serg@gmail.com>
2022-08-20 14:21:39 +0300

    add test 2

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -26,13 +26,6 @@ func RunCmd(cmd []string, env Environment) (returnCode int) {
 	envGlobalMap := make(map[string]string, len(envGlobal))
 	for _, elem := range envGlobal {
 		envStringSlice := strings.Split(elem, "=")
-		if len(envStringSlice) == 1 {
-			envGlobalMap[envStringSlice[0]] = ""
-			continue
-		}
-		if len(envStringSlice) < 2 {
-			continue
-		}
 		envGlobalMap[envStringSlice[0]] = envStringSlice[1]
 	}
 
@@ -61,17 +54,15 @@ func RunCmd(cmd []string, env Environment) (returnCode int) {
 		fmt.Println(err)
 		return 1
 	}
+	var waitStatus syscall.WaitStatus
 	if err := execCmd.Wait(); err != nil {
-		var exitErr *exec.ExitError
-		if !errors.As(err, &exitErr) {
-			if status, ok := exitErr.Sys().(syscall.WaitStatus); ok {
-				fmt.Printf("Exit Status: %d", status.ExitStatus())
-				return status.ExitStatus()
-			}
-		} else {
-			fmt.Printf("execCmd.Wait: %v", err)
-			return 1
+		os.Stderr.WriteString(fmt.Sprintf("Error: %s\n", err.Error()))
+		var exitError *exec.ExitError
+		if errors.As(err, &exitError) {
+			waitStatus = exitError.Sys().(syscall.WaitStatus)
 		}
+	} else {
+		waitStatus = execCmd.ProcessState.Sys().(syscall.WaitStatus)
 	}
-	return
+	return waitStatus.ExitStatus()
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,6 +1,59 @@
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRunCmd(t *testing.T) {
+	env := Environment{"ENV1": EnvValue{"val1", false}, "ENV2": EnvValue{"", true}, "ENV3": EnvValue{"", false}}
+	retCode := RunCmd([]string{"./testdata/echo.sh"}, env)
+	require.Equal(t, retCode, 0)
+
+	retCode = RunCmd([]string{"/root/admin.sh"}, env)
+	require.Equal(t, retCode, 1)
+
+	retCode = RunCmd([]string{""}, env)
+	require.Equal(t, retCode, 1)
+
+	retCode = RunCmd(nil, env)
+	require.Equal(t, retCode, 1)
+
+	env = Environment{"ENV1": EnvValue{"abcd\x00dcba", false}}
+	retCode = RunCmd([]string{"./testdata/echo.sh"}, env)
+	require.Equal(t, retCode, 1)
+
+	// ...............................
+
+	rescueStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	env = Environment{
+		"HELLO": EnvValue{"\"hello\"", false},
+		"BAR":   EnvValue{"bar", false},
+		"FOO":   EnvValue{"   foo\nwith new line", false},
+		"UNSET": EnvValue{"", true},
+		"EMPTY": EnvValue{"", false},
+	}
+
+	os.Setenv("ADDED", "from original env")
+	retCode = RunCmd([]string{"./testdata/echo.sh", "arg1=1", "arg2=2"}, env)
+	require.Equal(t, retCode, 0)
+	w.Close()
+	out, _ := ioutil.ReadAll(r)
+	os.Stdout = rescueStdout
+
+	s := string(out)
+	expected := "HELLO is (\"hello\")\n" +
+		"BAR is (bar)\n" +
+		"FOO is (   foo\nwith new line)\n" +
+		"UNSET is ()\n" +
+		"ADDED is (from original env)\n" +
+		"EMPTY is ()\n" +
+		"arguments are arg1=1 arg2=2\n"
+	require.Equal(t, s, expected)
 }
