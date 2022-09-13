2f0e77d6d94781b3e51336ced06d7e8db4a1fe1c
julin.serg <julin.serg@gmail.com>
2022-09-13 22:59:11 +0300

    HW11

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,123 @@
 package main
 
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"sync"
+	"time"
+)
+
+func readRoutine(ctx context.Context, conn net.Conn, wg *sync.WaitGroup) {
+	defer wg.Done()
+	scanner := bufio.NewScanner(conn)
+OUTER:
+	for {
+		select {
+		case <-ctx.Done():
+			break OUTER
+		default:
+			if !scanner.Scan() {
+				log.Printf("CANNOT SCAN")
+				break OUTER
+			}
+			text := scanner.Text()
+			log.Printf("From server: %s", text)
+		}
+	}
+	log.Printf("Finished readRoutine")
+}
+
+func writeRoutine(ctx context.Context, conn net.Conn, wg *sync.WaitGroup, stdin chan string) {
+	defer wg.Done()
+	//scanner := bufio.NewScanner(os.Stdin)
+OUTER:
+	for {
+		select {
+		case <-ctx.Done():
+			break OUTER
+		case str := <-stdin:
+			//if !scanner.Scan() {
+			//	break OUTER
+			//}
+			//str := scanner.Text()
+			log.Printf("To server %v\n", str)
+
+			conn.Write([]byte(fmt.Sprintf("%s\n", str)))
+		}
+
+	}
+	log.Printf("Finished writeRoutine")
+}
+
+func stdinScan() chan string {
+	out := make(chan string)
+	go func() {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			out <- scanner.Text()
+		}
+		if scanner.Err() != nil {
+			close(out)
+		}
+	}()
+	return out
+}
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+
+	client := NewTelnetClient("127.0.0.1:4242", 10*time.Second, os.Stdin, os.Stdout)
+	defer client.Close()
+
+	//ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+
+	err := client.Connect()
+	if err != nil {
+		log.Fatalf("Cannot connect: %v", err)
+	} else {
+		log.Println("Connected")
+	}
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		for {
+			err := client.Send()
+			if err != nil {
+				log.Fatalf("Error send: %v", err)
+			}
+			time.Sleep(10 * time.Microsecond)
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		for {
+			client.Receive()
+			if err != nil {
+				log.Fatalf("Error receive: %v", err)
+			}
+			time.Sleep(10 * time.Microsecond)
+		}
+	}()
+
+	/*ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	wg.Add(1)
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				fmt.Println(ctx.Err()) // prints "context canceled"
+				client.Close()
+				log.Fatalln("Ctrl+D press")
+				stop()
+			}
+		}
+	}()*/
+
+	wg.Wait()
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
 	"io"
+	"log"
+	"net"
 	"time"
 )
 
@@ -12,9 +15,58 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type TelnetClientImpl struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
+	cancel  context.CancelFunc
+}
+
+func (tc *TelnetClientImpl) Connect() error {
+	var err error
+	tc.conn, err = net.DialTimeout("tcp", tc.address, tc.timeout)
+	return err
+}
+
+func (tc *TelnetClientImpl) Send() error {
+	buffer := make([]byte, 1024)
+	numBytes, err := tc.in.Read(buffer)
+	if err != nil {
+		return err
+	}
+	if numBytes <= 0 {
+		return nil
+	}
+
+	numBytes, errNet := tc.conn.Write(buffer[:numBytes])
+	if numBytes > 0 {
+		log.Printf("To server %v\n", buffer[:numBytes])
+	}
+	return errNet
+}
+
+func (tc *TelnetClientImpl) Receive() error {
+	buffer := make([]byte, 1024)
+	numBytes, err := tc.conn.Read(buffer)
+	if err != nil {
+		return err
+	}
+	if numBytes <= 0 {
+		return nil
+	}
+	log.Printf("From server %v\n", buffer[:numBytes])
+	_, err = tc.out.Write(buffer[:numBytes])
+	return err
+}
+
+func (tc *TelnetClientImpl) Close() error {
+	return tc.conn.Close()
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
-	return nil
+	return &TelnetClientImpl{address: address, timeout: timeout, in: in, out: out}
 }
 
 // Place your code here.
