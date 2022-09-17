38dfa0d6ef53d2149d98aacde0a09625daec918e
julin.serg <julin.serg@gmail.com>
2022-09-17 10:42:51 +0300

    refactoring

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"io"
-	"log"
 	"net"
 	"time"
 )
@@ -29,7 +28,7 @@ type TelnetClientImpl struct {
 	receiveRoutineIsStart bool
 }
 
-func reader(r io.Reader, input chan string, errorCh chan error) {
+func reader(r io.Reader, inputCh chan string, errorCh chan error) {
 	rb := bufio.NewReader(r)
 	for {
 		dataStr, err := rb.ReadString('\n')
@@ -38,65 +37,44 @@ func reader(r io.Reader, input chan string, errorCh chan error) {
 			return
 		}
 		if len(dataStr) == 0 {
-			input <- ""
+			inputCh <- ""
 			return
 		}
-		input <- dataStr
+		inputCh <- dataStr
+	}
+}
+
+func waitReadAndSend(isStartReader *bool, r io.Reader, w io.Writer, inputCh chan string, errorCh chan error) error {
+	if !*isStartReader {
+		*isStartReader = true
+		go reader(r, inputCh, errorCh)
+	}
+
+	for {
+		select {
+		case data := <-inputCh:
+			_, err := w.Write([]byte(data))
+			return err
+		case e := <-errorCh:
+			return e
+		case <-time.After(1 * time.Second):
+			return nil
+		}
 	}
 }
 
 func (tc *TelnetClientImpl) Connect() error {
 	var err error
 	tc.conn, err = net.DialTimeout("tcp", tc.address, tc.timeout)
-
-	tc.inputChNet = make(chan string)
-	tc.errorChNet = make(chan error)
-	tc.inputChStdIn = make(chan string)
-	tc.errorChStdIn = make(chan error)
-
 	return err
 }
 
 func (tc *TelnetClientImpl) Send() error {
-	if !tc.sendRoutineIsStart {
-		tc.sendRoutineIsStart = true
-		go reader(tc.in, tc.inputChStdIn, tc.errorChStdIn)
-	}
-
-	for {
-		select {
-		case data := <-tc.inputChStdIn:
-			numBytes, err := tc.conn.Write([]byte(data))
-			if numBytes > 0 {
-				log.Printf("To server %v\n", data)
-			}
-			return err
-		case e := <-tc.errorChStdIn:
-			return e
-		case <-time.After(1 * time.Second):
-			return nil
-		}
-	}
+	return waitReadAndSend(&tc.sendRoutineIsStart, tc.in, tc.conn, tc.inputChStdIn, tc.errorChStdIn)
 }
 
 func (tc *TelnetClientImpl) Receive() error {
-	if !tc.receiveRoutineIsStart {
-		tc.receiveRoutineIsStart = true
-		go reader(tc.conn, tc.inputChNet, tc.errorChNet)
-	}
-
-	for {
-		select {
-		case data := <-tc.inputChNet:
-			log.Printf("From server %v\n", data)
-			_, err := tc.out.Write([]byte(data))
-			return err
-		case e := <-tc.errorChNet:
-			return e
-		case <-time.After(1 * time.Second):
-			return nil
-		}
-	}
+	return waitReadAndSend(&tc.receiveRoutineIsStart, tc.conn, tc.out, tc.inputChNet, tc.errorChNet)
 }
 
 func (tc *TelnetClientImpl) Close() error {
@@ -104,7 +82,14 @@ func (tc *TelnetClientImpl) Close() error {
 }
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	return &TelnetClientImpl{address: address, timeout: timeout, in: in, out: out}
+	return &TelnetClientImpl{
+		address: address, timeout: timeout,
+		in: in, out: out,
+		inputChNet:   make(chan string),
+		errorChNet:   make(chan error),
+		inputChStdIn: make(chan string),
+		errorChStdIn: make(chan error),
+	}
 }
 
 // Place your code here.
